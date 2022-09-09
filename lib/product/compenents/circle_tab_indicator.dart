88bd4c818469ab9874b83a2e13084e4f035cc41d
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-09-09 15:14:10 +0300

    create product circle tab indicator

---
+++ lib/product/compenents/circle_tab_indicator.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CircleTabIndicator extends Decoration {
+  final Color color;
+  double radius;
+
+  CircleTabIndicator({required this.color, required this.radius});
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return _CirclePainter(color: color, radius: radius);
+  }
+}
+
+class _CirclePainter extends BoxPainter {
+  final Color color;
+  double radius;
+
+  _CirclePainter({required this.color, required this.radius});
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    Paint paint = Paint();
+    paint.color = color;
+    paint.isAntiAlias = true;
+    final Offset circleOffset = Offset(
+      configuration.size!.width / 2 - radius / 2,
+      configuration.size!.height - radius,
+    );
+    canvas.drawCircle(offset + circleOffset, radius, paint);
+  }
+}
