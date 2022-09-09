bbb4938e257d13b4352a52fc7dff72964a4e3355
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-09-09 13:59:06 +0300

    create bottombar

---
+++ lib/screen/home/view/home_view.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:travelapp/screen/home/viewmodel/home_viewmodel.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = Provider.of<HomeViewModel>(context);
+    return Scaffold(
+      body: viewModel.pages[viewModel.currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: viewModel.onTapBottomBar,
+        currentIndex: viewModel.currentIndex,
+        selectedItemColor: Colors.black54,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        elevation: 0,
+        items: [
+          BottomNavigationBarItem(label: "Ana Sayfa", icon: Icon(Icons.apps)),
+          BottomNavigationBarItem(label: "Bar", icon: Icon(Icons.explore)),
+          BottomNavigationBarItem(label: "Ara", icon: Icon(Icons.search)),
+          BottomNavigationBarItem(label: "Profil", icon: Icon(Icons.person)),
+        ],
+      ),
+    );
+  }
+}
