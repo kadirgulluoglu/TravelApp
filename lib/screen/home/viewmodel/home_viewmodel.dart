836dca6f3739aaf149d9381d2c48bb5905190ee7
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-09-09 16:47:40 +0300

    Update home_viewmodel.dart

--- lib/screen/home/viewmodel/home_viewmodel.dart
+++ lib/screen/home/viewmodel/home_viewmodel.dart
@@ -1,27 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:travelapp/screen/homepage/view/home_page_view.dart';
 
 class HomeViewModel with ChangeNotifier {
   int _currentIndex = 0;
-  late List _pages;
 
-  List get pages => _pages;
-
-  HomeViewModel() {
-    _pages = [
-      Center(
-          child: Text(
-        "Ana Sayfa",
-        style: TextStyle(fontSize: 45),
-      )),
-      Center(
-          child: Text(
-        "Keşfet",
-        style: TextStyle(fontSize: 45),
-      )),
-      Container(color: Colors.redAccent),
-      Container(color: Colors.blueGrey),
-    ];
-  }
+  HomeViewModel() {}
   int get currentIndex => _currentIndex;
 
   set currentIndex(int value) {
