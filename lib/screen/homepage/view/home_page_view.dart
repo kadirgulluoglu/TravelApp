1408ec84c5b23452cfb0464a289c78c4d2bbfa85
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-09-09 16:47:52 +0300

    update home page view

---
+++ lib/screen/homepage/view/home_page_view.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:travelapp/init/theme/colors.dart';
+import 'package:travelapp/product/compenents/large_text.dart';
+import 'package:travelapp/product/compenents/text.dart';
+
+import '../../../product/compenents/circle_tab_indicator.dart';
+
+class HomePageView extends StatefulWidget {
+  const HomePageView({Key? key}) : super(key: key);
+
+  @override
+  State<HomePageView> createState() => _HomePageViewState();
+}
+
+class _HomePageViewState extends State<HomePageView>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+  Map<String, String> image = {
+    "balloning": "Balon Turu",
+    "hiking": "Dağ Yürüyüşü",
+    "snorkling": "Dalış",
+    "kayaking": "Kayak",
+  };
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 70, left: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(Icons.menu, size: 30, color: Colors.black54),
+                Expanded(child: Container()),
+                Container(
+                  margin: EdgeInsets.only(right: 20),
+                  width: 50,
+                  height: 50,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10), // Image border
+                    child: SizedBox.fromSize(
+                      size: Size.fromRadius(10), // Image radius
+                      child: Image.network('https://picsum.photos/200',
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.only(left: 20),
+              child: CustomLargeText(text: "Dünyayı Keşfet")),
+          SizedBox(height: 10),
+          Container(
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: TabBar(
+                labelPadding: EdgeInsets.only(left: 20, right: 20),
+                labelColor: Colors.black,
+                controller: _tabController,
+                unselectedLabelColor: Colors.grey,
+                indicatorSize: TabBarIndicatorSize.label,
+                isScrollable: true,
+                indicator:
+                    CircleTabIndicator(radius: 4, color: CustomColor.mainColor),
+                tabs: [
+                  Tab(text: "Yerler"),
+                  Tab(text: "Seyahat"),
+                  Tab(text: "Diğerleri"),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 20),
+            height: 270,
+            width: double.maxFinite,
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 3,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      padding: EdgeInsets.only(right: 15, top: 10),
+                      width: 200,
+                      height: 300,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10), // Image border
+                        child: SizedBox.fromSize(
+                          size: Size.fromRadius(30), // Image radius
+                          child: Image.network(
+                              'https://picsum.photos/30' + index.toString(),
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                Text("hi asdsadsad"),
+                Text("hi sadasd"),
+              ],
+            ),
+          ),
+          SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CustomLargeText(text: "Daha fazlasını Keşfet", size: 22),
+                CustomText(text: "See all", color: CustomColor.textColor1)
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            height: 100,
+            margin: EdgeInsets.only(left: 20),
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: image.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.only(right: 40),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 80,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: ClipRRect(
+                            borderRadius:
+                                BorderRadius.circular(10), // Image border
+                            child: SizedBox.fromSize(
+                              size: Size.fromRadius(30), // Image radius
+                              child: Image.asset(
+                                  'assets/images/${image.keys.elementAt(index)}.png',
+                                  fit: BoxFit.cover),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: CustomText(
+                            text: image.values.elementAt(index),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                }),
+          )
+        ],
+      ),
+    );
+  }
+}
