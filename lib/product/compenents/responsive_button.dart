3dc16c69a232b3b9c3f50af5981b75448a84ccd0
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-09-13 10:48:44 +0300

    add onpressed feature

--- lib/product/compenents/responsive_button.dart
+++ lib/product/compenents/responsive_button.dart
@@ -5,23 +5,28 @@ import '../../init/theme/colors.dart';
 class ResponsiveButton extends StatelessWidget {
   final bool? isResponsive;
   final double? width;
+  final VoidCallback? onPressed;
 
-  ResponsiveButton({Key? key, this.isResponsive, this.width}) : super(key: key);
+  ResponsiveButton({Key? key, this.isResponsive, this.width, this.onPressed})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: 60,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: CustomColor.mainColor,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset("assets/images/button-one.png"),
-        ],
+    return InkWell(
+      onTap: onPressed ?? () {},
+      child: Container(
+        width: width,
+        height: 60,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: CustomColor.mainColor,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/button-one.png"),
+          ],
+        ),
       ),
     );
   }
