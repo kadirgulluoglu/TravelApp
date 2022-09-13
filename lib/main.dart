fa23aef21a0d1ba1ea6e7997684f610fbd7203be
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-09-13 15:22:31 +0300

    routeing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:denemefirebaseauth/screens/home/view/home_view.dart';
+import 'package:denemefirebaseauth/screens/home/viewmodel/home_viewmodel.dart';
 import 'package:denemefirebaseauth/screens/onboarding/view/onboarding_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'locator.dart';
 
@@ -20,8 +23,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'TravelApp',
-        home: rememberme ? const OnBoardingView() : const OnBoardingView());
+      debugShowCheckedModeBanner: false,
+      title: 'TravelApp',
+      home: rememberme
+          ? const OnBoardingView()
+          : ChangeNotifierProvider(
+              create: (context) => HomeViewModel(),
+              child: const HomeView(),
+            ),
+    );
   }
 }
