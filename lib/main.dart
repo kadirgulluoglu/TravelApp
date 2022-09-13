beefcb90b77bb0f8f83f209c1746b782fefb6b7c
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-09-13 10:55:20 +0300

    remove unnecessary packet

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:denemefirebaseauth/screens/auth/viewmodel/auth_viewmodel.dart';
 import 'package:denemefirebaseauth/screens/onboarding/view/onboarding_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'locator.dart';
-import 'screens/auth/view/login_view.dart';
 
 bool rememberme = true;
 void main() async {
