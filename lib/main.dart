c50bb0f15bf8e5719a53b53097306542b064775c
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-09-14 17:59:09 +0300

    route operations on the home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:denemefirebaseauth/screens/auth/view/login_view.dart';
+import 'package:denemefirebaseauth/screens/auth/viewmodel/auth_viewmodel.dart';
 import 'package:denemefirebaseauth/screens/home/view/home_view.dart';
 import 'package:denemefirebaseauth/screens/home/viewmodel/home_viewmodel.dart';
 import 'package:denemefirebaseauth/screens/onboarding/view/onboarding_view.dart';
@@ -8,11 +10,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'locator.dart';
 
 bool rememberme = true;
+bool show = false;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   final prefs = await SharedPreferences.getInstance();
-  rememberme = prefs.getBool('BeniHatirla') ?? false;
+  rememberme = prefs.getBool('RememberMe') ?? false;
+  show = prefs.getBool('ON_BOARDING') ?? true;
   setupLocator();
   runApp(const MyApp());
 }
@@ -25,12 +29,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'TravelApp',
-      home: rememberme
+      home: show
           ? const OnBoardingView()
-          : ChangeNotifierProvider(
-              create: (context) => HomeViewModel(),
-              child: const HomeView(),
-            ),
+          : rememberme
+              ? ChangeNotifierProvider(
+                  create: (context) => HomeViewModel(),
+                  child: const HomeView(),
+                )
+              : ChangeNotifierProvider(
+                  create: (context) => AuthViewModel(),
+                  child: const LoginView(),
+                ),
     );
   }
 }
