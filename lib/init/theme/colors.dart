f3003a9a74e4a71452a7c7283cd031759f02dc79
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-09-14 17:58:28 +0300

    made const

--- lib/init/theme/colors.dart
+++ lib/init/theme/colors.dart
@@ -1,11 +1,11 @@
 import 'dart:ui';
 
 class CustomColor {
-  static final Color textColor1 = Color(0xFF989acd);
-  static final Color textColor2 = Color(0xFF878593);
-  static final Color bigTextColor = Color(0xFF2e2e31);
-  static final Color mainColor = Color(0xFF5d69b3);
-  static final Color starColor = Color(0xFFe7bb4e);
-  static final Color mainTextColor = Color(0xFFababad);
-  static final Color buttonBackground = Color(0xFFf1f1f9);
+  static const Color textColor1 = Color(0xFF989acd);
+  static const Color textColor2 = Color(0xFF878593);
+  static const Color bigTextColor = Color(0xFF2e2e31);
+  static const Color mainColor = Color(0xFF5d69b3);
+  static const Color starColor = Color(0xFFe7bb4e);
+  static const Color mainTextColor = Color(0xFFababad);
+  static const Color buttonBackground = Color(0xFFf1f1f9);
 }
