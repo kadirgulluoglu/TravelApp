d34884d21e87aebe54f72cd139184a006c16e306
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-09-23 18:10:25 +0300

    add netwrok enum

--- lib/product/enum/image_enum.dart
+++ lib/product/enum/image_enum.dart
@@ -1,10 +1,11 @@
 enum AssetsEnum {
   button("button"),
+  network("network"),
   balloning("balloning");
 
   String toPng() => 'assets/images/$name.png';
   String toJpg() => 'assets/images/$name.jpg';
-  String toLottie() => 'assets/lottie/$name.lottie';
+  String toLottie() => 'assets/lottie/$name.json';
 
   final String name;
   const AssetsEnum(this.name);
